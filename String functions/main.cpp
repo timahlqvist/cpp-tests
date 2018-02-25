e380dd5605dae1ccbef0c75587d38ff75ef6ee22
Tim Ahlqvist <tim@lioar.se>
2018-02-25 13:37:48 +0100

    String func

---
+++ String functions/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name = "Lioar";
+    string fname = "Tim";
+    string lname = "Ahlqvist";
+
+    cout << name.length() << endl;
+    cout << lname.substr(3) << endl;
+    cout << fname.at(2) << endl;
+
+    return 0;
+}
