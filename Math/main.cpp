2e2e295c76bfec2734bdd00e960866f4b6c6603a
Tim Ahlqvist <tim@lioar.se>
2018-02-27 20:52:20 +0100

    cmath

---
+++ Math/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    cout << pow(5, 3) << endl; // 5 upphöjt till 3
+    cout << sqrt(9) << endl;   // kvadratroten av 9
+
+    float x = 2.14;
+
+    cout << ceil(x) << endl;   // Avrundning uppåt
+    cout << floor(x) << endl;  // Avrundning neråt
+
+    return 0;
+}
