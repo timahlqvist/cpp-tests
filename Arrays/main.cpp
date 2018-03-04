ea765a31a223e2005bc5375bdd9fa5f2aa7575cc
Tim Ahlqvist <tim@lioar.se>
2018-03-04 14:25:50 +0100

    array

---
+++ Arrays/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int age[10];
+
+    age[0] = 10;
+    age[1] = 15;
+    age[2] = 2;
+    age[3] = 83;
+    age[4] = 42;
+    age[5] = 34;
+    age[6] = 49;
+    age[7] = 39;
+    age[8] = 4;
+    age[9] = 8;
+
+    cout << age[8] << endl;
+
+}
