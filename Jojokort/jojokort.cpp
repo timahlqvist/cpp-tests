8c2fde2159f1b8f41d881d506ea5b715868ebdfd
Tim Ahlqvist <tim@lioar.se>
2018-03-21 19:42:05 +0100

    Jojokort

--- Jojokort/jojokort.cpp
+++ Jojokort/jojokort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -6,35 +7,51 @@ class Card{
     public:
         bool pay;
 
-        int serialNumber;
-
         string owner;
         string expandIn;
         string registeredIn;
 
     private:
-        float money;
+        int money;
+        int serialNumber;
 
     public:
+        void addMoney(int addMoney){
+            money += addMoney;
+        }
         void printMoney(){
             cout << "You have: " << money << " money on your Jojo card!" << endl;
         }
-        void addMoney(addMoney){
-            money += addMoney;
+        void setSerial(){
+            for(int i; i <= 10; i++){
+                serialNumber += rand() % 10;
+            }
         }
-
+        void printSerial(){
+            cout << serialNumber << endl;
+        }
 };
 
 int main(){
 
-    Card timsCard;
+    int addMoney;
+
+    Card timcard;
+
+    timcard.setSerial();
 
     cout << "How much money do you want to transfer to your Jojo card?" << endl;
     cin >> addMoney;
 
-    timsCard.addMoney(addMoney);
-    timsCard.printMoney();
-
+    timcard.addMoney(addMoney);
+    timcard.registeredIn = "10/9-2006";
+    timcard.owner = "Tim Ahlqvist";
+    timcard.expandIn = "500 years";
+    timcard.printMoney();
+    timcard.printSerial();
+    cout << timcard.registeredIn << endl;
+    cout << timcard.owner << endl;
+    cout << timcard.expandIn << endl;
 
     return 0;
 }
